1cd2776b22dd7366dec595488e474085bf2d454f
H3nryK <henrykimani89@gmail.com>
2024-11-20 05:00:29 +0300

    Backend works harmoniously

--- EndoCare/settings.py
+++ EndoCare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y5(mx3#)08v*%r+&xd=$ui)@qt64^6f4-z&j^vhv=+@7bbal1y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.2', '127.0.0.1']
 
 
 # Application definition
@@ -63,6 +63,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:8080",  
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'EndoCare.urls'
 
--- core/firebase.py
+++ core/firebase.py
@@ -1,6 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, auth
+import os
 
 # Load Firebase credentials
-cred = credentials.Certificate("firebase-credentials.json")
+cred_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "firebase-credentials.json")
+cred = credentials.Certificate(cred_file_path)
 firebase_admin.initialize_app(cred)
